9c45e70615a2ac58c50550a6729c873a3c9e6ecc
Patricio104 <patriciojuarez104@gmail.com>
2021-05-18 09:05:44 -0500

    DAO

---
+++ app/src/main/java/com/patricio/pelisapp/data/local/MovieDao.kt
@@ -0,0 +1,17 @@
+package com.patricio.pelisapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.patricio.pelisapp.data.model.MovieEntity
+
+@Dao
+interface MovieDao {
+    @Query("SELECT * FROM movieentity")
+    suspend fun getAllMovies(): List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovie(movie: MovieEntity)
+
+}
